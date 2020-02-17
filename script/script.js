2ed7392af78ff8000d2d86abcc35605aa02c9506
unknown <bev030997@gmail.com>
2020-02-17 23:34:17 +0300

    easy task: timer done

---
+++ script/script.js
@@ -0,0 +1,61 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    // Timer
+    function countTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+
+            function addZero(number) {
+                let strNumber = number.toString();
+
+                if (strNumber.length === 1) {
+                    return `0` + strNumber;
+                } else {
+                    return strNumber;
+                }
+            }
+
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = addZero(Math.floor(timeRemaining % 60)),
+                minutes = addZero(Math.floor((timeRemaining / 60) % 60)),
+                hours = addZero(Math.floor(timeRemaining / 60 / 60));
+            // day = Math.floor(timeRemaining / 60 / 60 / 24);
+
+            return {
+                timeRemaining,
+                hours,
+                minutes,
+                seconds
+            };
+        }
+
+        function updateClock() {
+            let timer = getTimeRemaining();
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+
+            if (timer.timeRemaining > 0) {
+                //setTimeout(updateClock, 1000);
+                setInterval(updateClock, 1000);
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+
+        }
+
+        updateClock();
+
+    }
+
+    countTimer('18 feb 2020');
+});
