4b0b8b1c92775afde5907415f47cdc0d690a3774
unknown <bev030997@gmail.com>
2020-02-25 20:32:51 +0300

    easy task done && add files

--- hard/script/script.js
+++ hard/script/script.js
@@ -56,7 +56,7 @@ window.addEventListener('DOMContentLoaded', function () {
         clockId = setInterval(updateClock, 1000);
     };
 
-    countTimer('12 mar 2020');
+    countTimer('24 feb 2020');
 
 
     // menu
@@ -342,13 +342,8 @@ window.addEventListener('DOMContentLoaded', function () {
     switchImg();
 
     // calculator
-    const calculator = (price) => {
-        const calcBlock = document.querySelector('.calc-block'),
-            calcType = document.querySelector('.calc-type'),
-            calcSquare = document.querySelector('.calc-square'),
-            calcDay = document.querySelector('.calc-day'),
-            calcCount = document.querySelector('.calc-count'),
-            totalValue = document.getElementById('total');
+    const calculator = () => {
+        const calcBlock = document.querySelector('.calc-block');
 
         calcBlock.addEventListener('input', (event) => {
             const target = event.target;
@@ -357,41 +352,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 target.value = target.value.replace(/[^\d,]/g, '');
             }
         });
-
-
-        const countSum = (price = 100) => {
-            let total = 0,
-                countValue = 1,
-                dayValue = 1;
-            const typeValue = calcType.options[calcType.selectedIndex].value,
-                squareValue = +calcSquare.value;
-
-            if (+calcCount.value > 1) {
-                countValue += (calcCount.value - 1) / 10;
-            }
-
-            if (calcDay.value && calcDay.value < 5) {
-                dayValue *= 2;
-            } else if (calcDay.value && calcDay.value < 10) {
-                dayValue *= 1.5;
-            }
-
-            if (typeValue && squareValue) {
-                total = price * typeValue * squareValue * countValue * dayValue;
-            }
-
-            totalValue.textContent = total;
-        };
-
-        calcBlock.addEventListener('change', (event) => {
-            const target = event.target;
-
-            if (target.matches('.calc-type') || target.matches('.calc-square') ||
-                target.matches('.calc-day') || target.matches('.calc-count')) {
-                countSum();
-            }
-
-        });
     };
-    calculator(100);
+
+    calculator();
 });
